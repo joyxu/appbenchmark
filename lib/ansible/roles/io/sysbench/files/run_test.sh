fdd2d2abb9eaf466813ef4f3f14b77bca3e90c41
wanglei <seilwang@163.com>
2017-08-30 06:45:11 +0000

    modify main.yml

--- lib/ansible/roles/io/sysbench/files/run_test.sh
+++ lib/ansible/roles/io/sysbench/files/run_test.sh
@@ -5,12 +5,6 @@ if [ -z "$(which sysbench 2>/dev/null)" ] ; then
     exit 0
 fi
 
-IOTEST_DIR="/tmp/sysbench/"
-
-if [ ! -d "${IOTEST_DIR}" ] ; then
-    echo "Please mount this ${IOTEST_DIR} to target disk which will be tested"
-    exit 0
-fi
 
 CUR_DIR=$(cd `dirname $0`; pwd)
 
@@ -21,9 +15,7 @@ FILE_SIZE="20G"
 TEST_MODE="rndrw" 
 #seqwr seqrewr seqrd rndrd rndwr rndrw
 
-pushd ${IOTEST_DIR} > /dev/null
 sysbench fileio --file-total-size=${FILE_SIZE} prepare
-sysbench fileio --file-total-size=${FILE_SIZE} --file-test-mode=${TEST_MODE} --time=300 --max-requests=0 --num-threads=100 run
+sysbench fileio --file-total-size=${FILE_SIZE} --file-test-mode=${TEST_MODE} --time=300 --max-requests=0  run >>result.log
 sysbench fileio --file-total-size=${FILE_SIZE} cleanup
 
-popd > /dev/null
