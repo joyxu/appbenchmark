55e795c317ce135f146badb7e091372efe2394c0
wanglei <seilwang@163.com>
2017-08-30 05:42:30 +0000

    add the ansible roles for io/sysbench

---
+++ lib/ansible/roles/io/sysbench/files/run_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z "$(which sysbench 2>/dev/null)" ] ; then
+    echo "Please install sysbench firstly !"
+    exit 0
+fi
+
+IOTEST_DIR="/tmp/sysbench/"
+
+if [ ! -d "${IOTEST_DIR}" ] ; then
+    echo "Please mount this ${IOTEST_DIR} to target disk which will be tested"
+    exit 0
+fi
+
+CUR_DIR=$(cd `dirname $0`; pwd)
+
+echo "Start fio test......"
+
+FILE_SIZE="20G"
+
+TEST_MODE="rndrw" 
+#seqwr seqrewr seqrd rndrd rndwr rndrw
+
+pushd ${IOTEST_DIR} > /dev/null
+sysbench fileio --file-total-size=${FILE_SIZE} prepare
+sysbench fileio --file-total-size=${FILE_SIZE} --file-test-mode=${TEST_MODE} --time=300 --max-requests=0 --num-threads=100 run
+sysbench fileio --file-total-size=${FILE_SIZE} cleanup
+
+popd > /dev/null
