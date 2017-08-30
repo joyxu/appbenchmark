8d3aeaeac4c0d3c2fc4e7d9ae4e9fca5ff3f759e
wanglei <seilwang@163.com>
2017-08-30 07:39:08 +0000

    modify run_test.sh

--- lib/ansible/roles/io/sysbench/files/run_test.sh
+++ lib/ansible/roles/io/sysbench/files/run_test.sh
@@ -10,12 +10,15 @@ CUR_DIR=$(cd `dirname $0`; pwd)
 
 echo "Start fio test......"
 
-FILE_SIZE="20G"
+FILE_SIZE="120G"
 
 TEST_MODE="rndrw" 
+TIME= 300
+
 #seqwr seqrewr seqrd rndrd rndwr rndrw
 
 sysbench fileio --file-total-size=${FILE_SIZE} prepare
-sysbench fileio --file-total-size=${FILE_SIZE} --file-test-mode=${TEST_MODE} --time=300 --max-requests=0  run >>result.log
+nohup sysbench fileio --file-total-size=${FILE_SIZE} --file-test-mode=${TEST_MODE} --time=${TIME} --max-requests=0 run  2>&1 &
+sleep ${TIME}
 sysbench fileio --file-total-size=${FILE_SIZE} cleanup
 
