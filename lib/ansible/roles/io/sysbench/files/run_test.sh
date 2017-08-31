8ad8254a0c87a599e6580e5840883bb954718beb
wanglei <seilwang@163.com>
2017-08-31 03:10:27 +0000

    add io sysbench test modes

--- lib/ansible/roles/io/sysbench/files/run_test.sh
+++ lib/ansible/roles/io/sysbench/files/run_test.sh
@@ -5,20 +5,26 @@ if [ -z "$(which sysbench 2>/dev/null)" ] ; then
     exit 0
 fi
 
-
 CUR_DIR=$(cd `dirname $0`; pwd)
-
+mkdir -p ${CUR_DIR}/result
+IOTEST_DIR="${CUR_DIR}/result"
 echo "Start fio test......"
 
-FILE_SIZE="120G"
+FILE_SIZE="20G"
 
-TEST_MODE="rndrw" 
-TIME= 300
+TEST_MODE=("rndrw" "rndwr" "rndrd" "seqrd" "seqrewr") 
+TIME=300
 
 #seqwr seqrewr seqrd rndrd rndwr rndrw
+pushd ${IOTEST_DIR} > /dev/null
 
-sysbench fileio --file-total-size=${FILE_SIZE} prepare
-nohup sysbench fileio --file-total-size=${FILE_SIZE} --file-test-mode=${TEST_MODE} --time=${TIME} --max-requests=0 run  2>&1 &
-sleep ${TIME}
+for var in "${TEST_MODE[@]}"
+do 
+	 sysbench fileio --file-total-size=${FILE_SIZE} prepare	
+	 nohup sysbench fileio --file-total-size=${FILE_SIZE} --file-test-mode=$var --time=${TIME} --max-requests=0 run >$var.result  2>&1 &
+	 sleep ${TIME}
+
+done
 sysbench fileio --file-total-size=${FILE_SIZE} cleanup
+popd > /dev/null
 
