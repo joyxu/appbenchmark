b389ab6290fcad3311ff5e3e4457ed1ebe9a1ce6
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-11-21 11:46:27 +0800

    fix merge conflict

--- lib/ansible/roles/io/fio/files/run_test.sh
+++ lib/ansible/roles/io/fio/files/run_test.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-if [ -z "$(which sysbench 2>/dev/null)" ] ; then
-    echo "Please install sysbench firstly !"
-    exit 0
-fi
-
-
-CUR_DIR=$(cd `dirname $0`; pwd)
-
-echo "Start fio test......"
-
-FILE_SIZE="120G"
-
-TEST_MODE="rndrw" 
-TIME= 300
-
-#seqwr seqrewr seqrd rndrd rndwr rndrw
-
-sysbench fileio --file-total-size=${FILE_SIZE} prepare
-nohup sysbench fileio --file-total-size=${FILE_SIZE} --file-test-mode=${TEST_MODE} --time=${TIME} --max-requests=0 run  2>&1 &
-sleep ${TIME}
-sysbench fileio --file-total-size=${FILE_SIZE} cleanup
-=======
 if [ -z "$(which fio 2>/dev/null)" ] ; then
     echo "Please install sysbench firstly !"
     exit 0
@@ -57,5 +34,4 @@ popd > /dev/null
 
 
 
->>>>>>> upstream/master
 
--- lib/ansible/roles/io/sysbench/files/run_test.sh
+++ lib/ansible/roles/io/sysbench/files/run_test.sh
@@ -4,20 +4,6 @@ if [ -z "$(which sysbench 2>/dev/null)" ] ; then
     echo "Please install sysbench firstly !"
     exit 0
 fi
-<<<<<<< HEAD
-
-CUR_DIR=$(cd `dirname $0`; pwd)
-mkdir -p ${CUR_DIR}/result
-IOTEST_DIR="${CUR_DIR}/result"
-echo "Start fio test......"
-
-FILE_SIZE="20G"
-
-TEST_MODE=("rndrw" "rndwr" "rndrd" "seqrd" "seqrewr") 
-TIME=300
-
-#seqwr seqrewr seqrd rndrd rndwr rndrw
-=======
 IP=`ifconfig eth0|sed -n 2p|awk  '{ print $2 }'|tr -d 'addr:'`;
 CUR_DIR=$(cd `dirname $0`; pwd)
 if [ -d "${CUR_DIR}/result" ]; then
@@ -30,20 +16,13 @@ FILE_SIZE="4G"
 TEST_MODE=("rndrw" "rndwr" "rndrd" "seqrd" "seqrewr" "seqwr") 
 TIME=60
 WAIT_CLEANUP=65
->>>>>>> upstream/master
 pushd ${IOTEST_DIR} > /dev/null
 
 for var in "${TEST_MODE[@]}"
 do 
 	 sysbench fileio --file-total-size=${FILE_SIZE} prepare	
-<<<<<<< HEAD
-	 nohup sysbench fileio --file-total-size=${FILE_SIZE} --file-test-mode=$var --time=${TIME} --max-requests=0 run >$var.result  2>&1 &
-	 sleep ${TIME}
-
-=======
 	 nohup sysbench fileio --file-total-size=${FILE_SIZE} --file-test-mode=$var --time=${TIME} --max-requests=0 run >>result_${IP}  2>&1 &
 	 sleep ${WAIT_CLEANUP}
->>>>>>> upstream/master
 done
 sysbench fileio --file-total-size=${FILE_SIZE} cleanup
 popd > /dev/null
